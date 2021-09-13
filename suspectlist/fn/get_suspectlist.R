5f62bd4b34834b3e094bb36b852e5e98d4b59de0
benjaminplace <benjamin.place@nist.gov>
2021-09-13 17:06:07 -0400

    Updated with new suspect list.

--- suspectlist/fn/get_suspectlist.R
+++ suspectlist/fn/get_suspectlist.R
@@ -1,5 +1,5 @@
 get_suspectlist <- function(destfile = paste("PFAS Suspect List", Sys.Date(), ".xlsx", sep = "")) {
-  url <- "https://data.nist.gov/od/ds/mds2-2387/PFAS%20Suspect%20List_v1_2.xlsx"
+  url <- "https://data.nist.gov/od/ds/mds2-2387/PFAS%20Suspect%20List.xlsx"
   download.file(url, destfile, mode = "wb")
   if (file.exists(destfile)) {
     print(paste("The PFAS Suspect List has been successfully downloaded to", destfile))
